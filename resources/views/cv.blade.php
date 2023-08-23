0389a5e2f12f56ab8201b31f4e35e56c7416dba3
Permana721 <sdharma721@gmail.com>
2023-08-23 11:51:42 +0700

    first commit

---
+++ resources/views/cv.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curriculum Vitae</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <style>
+  
+  
+  body {
+  background-color: rgb(0, 0, 0);
+  color: rgb(255, 255, 255);
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+@media (max-width: 768px) {    
+}
+
+@media (max-width: 480px) {   
+}
+
+img {
+    max-width: 100%;
+    height: auto;
+}
+
+
+
+.container {
+  padding: 20px;
+}
+
+.header {
+  text-align: center;
+  padding-bottom: 20px;
+  border-bottom: 2px solid rgb(255, 254, 254);
+  margin-bottom: 20px;
+}
+
+.section {
+  margin-bottom: 30px;
+}
+
+.section h2 {
+  color: rgb(255, 255, 255);
+  border-bottom: 2px solid rgb(255, 255, 255);
+  padding-bottom: 10px;
+  margin-bottom: 20px;
+}
+
+.section p {
+  margin: 5px 0;
+}
+
+
+
+  </style>
+</head>
+<body>
+  <div class="container">
+    <div class="header">
+      <h1>CV</h1>
+      <img src="https://awsimages.detik.net.id/community/media/visual/2019/02/19/42393387-9c5c-4be4-97b8-49260708719e.jpeg?w=600&q=90" alt="" width="200px" >
+    </div>
+    <div class="section">
+      <h2>Biodata Diri</h2>
+      <p>Nama: Hessel Christian</p>
+      <p>Tempat Tanggal Lahir: Bandung 6 Mei 2007</p>
+      <p>Jenis Kelamin: Laki-Laki</p>
+      <p>Alamat: Jln.Mangga no 119 Bandung</p>
+    </div>
+    <div class="section">
+      <h2>Sekolah</h2>
+      <p>SD Santa Melania</p>
+      <p>SMP Sumatra 40</p>
+      <P>SMKN 2 Bandung</p>
+    </div>
+    <div class="section">
+      <h2>Skill</h2>
+      <ul>
+        <li>Problem Solving</li>
+        <li>Make a Good Realitionship</li>
+        <li>Good a Team Work</li>
+      </ul>
+    </div>
+    <div class="section">
+      <h2>Hubungi</h2>
+      <p>0876345723825</p>
+      <P>jsfgjsgfugf@gmail.com</P>
+      <p>instagram: gtri_</p>
+    </div>
+  </div>
+</body>
+</html>
